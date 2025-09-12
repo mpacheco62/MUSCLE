e1ae161fec8b6dafb3ab4cffb69e2a73435a740a
Matias Pacheco <matias.pacheco.a@gmail.com>
2025-09-12 01:30:52 -0300

    Add initialization and first true implementatio of stress_eq function in CPB06 module

--- src/yield_criteria/CPB06.f90
+++ src/yield_criteria/CPB06.f90
@@ -1,29 +1,91 @@
 module mod_CPB06
     use, intrinsic :: iso_fortran_env
+    use tensors_types
     use mod_yield_criteria
     implicit none
     PRIVATE
 
     PUBLIC :: CPB06
     type, extends(Base_yield_critera) :: CPB06
+      real(real64), dimension(3,3) :: C1
+      real(real64), dimension(3) :: C2
+      real(real64) :: k
+      real(real64) :: a
+
     contains
         procedure :: stress_eq
+        procedure :: init
         ! procedure :: dstressEq_dstress => dstressEq_dstress_vm
         ! procedure :: ddstressEq_ddstress => ddstressEq_ddstress_vm
     end type CPB06
 
     contains
+    subroutine init(self,           &
+                    c11, c12, c13,  &
+                    c21, c22, c23,  &
+                    c31, c32, c33,  &
+                    c44, c55, c66,  &
+                    k, a)
+        implicit none
+        class(CPB06), intent(inout) :: self
+        real(real64), intent(in) :: c11, c12, c13, c21, c22, c23, c31, c32, c33, c44, c55, c66
+        real(real64), intent(in) :: k, a
+
+        self%C1 = reshape(  &
+                          [c11, c21, c31, &
+                           c12, c22, c32, &
+                           c13, c23, c33], &
+                          [3,3] )
+
+        self%C2 = [c44, c55, c66]
+        self%k = k
+        self%a = a
+
+    end subroutine init
+
+
     pure function stress_eq(self, stress) result(res)
         use tensors_types
+        use basic_operations, only : eigenvals
         implicit None
         class(CPB06), intent(in) :: self
         class(ten_3D2Osym), intent(in) :: stress
+
+        real(real64) :: k, a
+
+        type(ten_3D2Osym) :: sigma
         real(real64) :: res
         type(ten_3D2Osym) :: dev
+        real(real64), dimension(3) :: sigma_eig
+
+        real(real64) :: sxx, syy, szz, sxy, syz, sxz
+        real(real64), dimension(3):: gamma
+        real(real64) :: b
+
+        k = self%k
+        a = self%a
 
         dev = .dev. stress
-        res = (1.5D0*(dev .ddot. dev))**0.5
-        ! testing comment
+        sxx = dev%xx()*self%C1(1,1) + dev%yy()*self%C1(1,2) + dev%zz()*self%C1(1,3)
+        syy = dev%xx()*self%C1(2,1) + dev%yy()*self%C1(2,2) + dev%zz()*self%C1(2,3)
+        szz = dev%xx()*self%C1(3,1) + dev%yy()*self%C1(3,2) + dev%zz()*self%C1(3,3)
+        sxy = dev%xy()*self%C2(1)
+        syz = dev%yz()*self%C2(2)
+        sxz = dev%xz()*self%C2(3)
+
+        call sigma%init(xx=sxx, yy=syy, zz=szz, xy=sxy, yz=syz, xz=sxz)
+        sigma_eig = eigenvals(sigma)
+
+        res = sum((abs(sigma_eig) - k*sigma_eig)**a)**(1D0/a)
+
+        gamma(1) = (2D0*self%C1(1,1) - self%C1(1,2) - self%C1(1,3))/3D0
+        gamma(2) = (2D0*self%C1(2,1) - self%C1(2,2) - self%C1(2,3))/3D0
+        gamma(3) = (2D0*self%C1(3,1) - self%C1(3,2) - self%C1(3,3))/3D0
+
+        b = sum((abs(gamma) - k*gamma)**a)**(-1D0/a)
+
+        res = res*b
+
     end function stress_eq
 
   !   pure function dstressEq_dstress_vm(self, stress) result(res)
