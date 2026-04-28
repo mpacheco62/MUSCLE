42ed1c4dd9eff7a0673aae7e9801d6c7b3345c95
LuisMontoya <luis.montoya@usach.cl>
2026-04-28 12:26:58 -0400

    Add dstress_dep functions for NNL, RK, and VA viscoplastic models

--- src/plasticity/viscoplastic_laws/RK_model.f90
+++ src/plasticity/viscoplastic_laws/RK_model.f90
@@ -47,8 +47,12 @@ module mod_RK_viscoplastic
         real(real64) :: epdmax 
         real(real64) :: sig0   
         real(real64) :: m      
+        real(real64) :: epmin = 1D-6 
+        real(real64) :: epmax = 10000 
+
     contains
         procedure :: flow_stress => flow_RK
+        procedure :: dstress_dep => dstress_dep_RK
     end type RK_viscoplastic
 
 contains
@@ -89,5 +93,46 @@ contains
 
         res = B_val * (self%ep0 + ep)**(n_val) + sig_extra
     end function flow_RK
+    pure function dstress_dep_RK(self, ep, epd, dt) result(res)
+        class(RK_viscoplastic), intent(in) :: self
+        real(real64), intent(in) :: ep, epd, dt  ! ep = ep_old + dep
+        real(real64) :: res
+        
+        real(real64) :: n_val, B_val, factor_sig, log_arg_B, log_arg_n, epd_tmp
+        real(real64) :: dn_depd, dB_depd, dsig_extra_depd, inv_ln10
+        real(real64) :: K_base, term1_deriv, term2_deriv
+
+        epd_tmp = max(self%epmin, min(epd, self%epdmax * 0.99d0))
+        inv_ln10 = 1.0d0 / log(10.0d0)
+        
+        log_arg_n = log10(epd_tmp / self%epdmin)
+        n_val = self%n0 * max(0.0d0, (1.0d0 - self%D2 * log_arg_n))
+        
+        log_arg_B = max(log10(self%epdmax / epd_tmp), 0.01d0)
+        B_val = self%B0 * (log_arg_B**(-self%nu))
+        
+        factor_sig = 1.0d0 - self%D1 * log10(self%epdmax / epd_tmp)
+
+        K_base = self%ep0 + ep
+
+        dn_depd = self%n0 * (-self%D2) * (inv_ln10 / (epd_tmp * dt))
+
+        dB_depd = (self%B0 * self%nu * (log_arg_B**(-self%nu - 1.0d0))) * &
+                  (inv_ln10 / (epd_tmp * dt))
+
+        term1_deriv = (dB_depd * (K_base**n_val)) + &
+                      (B_val * (K_base**n_val) * log(K_base) * dn_depd)
+
+        if (factor_sig > 1.0d-10) then
+            term2_deriv = self%sig0 * self%m * (factor_sig**(self%m - 1.0d0)) * &
+                          (self%D1 * inv_ln10 / (epd_tmp * dt))
+        else
+            term2_deriv = 0.0d0
+        end if
+
+        res = term1_deriv + term2_deriv
+
+    end function dstress_dep_RK
+    
 
 end module mod_RK_viscoplastic
--- src/plasticity/viscoplastic_laws/VA_model.f90
+++ src/plasticity/viscoplastic_laws/VA_model.f90
@@ -19,45 +19,49 @@ module mod_VA_viscoplastic
 
 contains
 
-    pure function flow_VA(self, ep, epd) result(res)
-        class(VA_viscoplastic), intent(in) :: self
-        real(real64), intent(in) :: ep, epd
-        real(real64) :: res
-        real(real64) :: factor_rate
+pure function flow_VA(self, ep, epd) result(res)
+    class(VA_viscoplastic), intent(in) :: self
+    real(real64), intent(in) :: ep, epd
+    real(real64) :: res
+    real(real64) :: factor_rate, ep_eff, epd_eff
 
-        if (self%m /= 0.0) then
-            factor_rate = 1.0 - self%B1 * epd**(1.0/self%m) - self%B2
-        else
-            factor_rate = 1.0 - self%B2
-        end if
+    ep_eff = max(ep, 1.0d-10) 
+    epd_eff = max(epd, 1.0d-12)
 
-        res = self%B * ep**(self%n) * factor_rate + self%sig_u
-    end function flow_VA
+    if (self%m /= 0.0d0) then
+        factor_rate = 1.0d0 + self%B1 * (epd_eff**(1.0d0/self%m)) - self%B2
+    else
+        factor_rate = 1.0d0 - self%B2
+    end if
 
-    pure function dstress_dep_VA(self, ep, epd, dt) result(res)
-        class(VA_viscoplastic), intent(in) :: self
-        real(real64), intent(in) :: ep, epd, dt
-        real(real64) :: res
-        real(real64) :: ep_eff, epd_eff, factor_rate
-        real(real64) :: dsig_dep, dsig_depd
-        real(real64) :: d_factor_depd, hardening_part
+    res = self%B * (ep_eff**self%n) * factor_rate + self%sig_u
+end function flow_VA
 
-        ep_eff = max(ep, 1.0d-10)
-        epd_eff = max(epd, 1.0d-12)
+pure function dstress_dep_VA(self, ep, epd, dt) result(res)
+    class(VA_viscoplastic), intent(in) :: self
+    real(real64), intent(in) :: ep, epd, dt
+    real(real64) :: res
+    real(real64) :: ep_eff, epd_eff, factor_rate
+    real(real64) :: dsig_dep, dsig_depd, d_factor_depd, hardening_part
 
-        if (self%m /= 0.0d0) then
-            factor_rate = 1.0d0 - self%B1 * (epd_eff**(1.0d0/self%m)) - self%B2
-            d_factor_depd = - (self%B1 / self%m) * (epd_eff**(1.0d0/self%m - 1.0d0))
-        else
-            factor_rate = 1.0d0 - self%B2
-            d_factor_depd = 0.0d0
-        end if
+    ep_eff = max(ep, 1.0d-10)
+    epd_eff = max(epd, 1.0d-12)
 
-        hardening_part = self%B * (ep_eff**self%n)
-        dsig_dep = self%B * self%n * (ep_eff**(self%n - 1.0d0)) * factor_rate
-        dsig_depd = hardening_part * d_factor_depd
-        res = dsig_dep + (dsig_depd / dt)
+    if (self%m /= 0.0d0) then
+        factor_rate = 1.0d0 + self%B1 * (epd_eff**(1.0d0/self%m)) - self%B2
+        d_factor_depd = (self%B1 / self%m) * (epd_eff**(1.0d0/self%m - 1.0d0))
+    else
+        factor_rate = 1.0d0 - self%B2
+        d_factor_depd = 0.0d0
+    end if
 
-    end function dstress_dep_VA
+    hardening_part = self%B * (ep_eff**self%n)
+    
+    dsig_dep = self%B * self%n * (ep_eff**(self%n - 1.0d0)) * factor_rate
+    
+    dsig_depd = hardening_part * d_factor_depd
+    
+    res = dsig_dep + (dsig_depd / dt)
+end function dstress_dep_VA
 
 end module mod_VA_viscoplastic
